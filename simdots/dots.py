7381cbc55499ad097a69613a585030b74f2810f1
paolopoli1980 <paolopoli1980@gmail.com>
2021-07-25 10:15:07 +0200

    Add files via upload

--- simdots/dots.py
+++ simdots/dots.py
@@ -1,3 +1,4 @@
+import numpy as np
 class dot:
     def __init__(self,index):
         self.connections=[]
@@ -10,6 +11,7 @@ class dot:
 
     def sim(self,dots):
         stateindex=0
+        
         for i in range(len(self.connections)):
             stateindex+=((dots[self.connections[i]].maxstate+1)**(len(self.connections)-i-1))*dots[self.connections[i]].state   
         self.statenew=self.states[stateindex]
@@ -29,10 +31,77 @@ class simdot:
                 
             for el in dots:        
                 el.state=el.statenew
-            #print('********************')
+            
             digitlist=[]
             for el in dots:
-                #print(el.state, end=" ")
+                
                 digitlist.append(el.state)
             listofstates.append(digitlist)          
         return listofstates
+
+class qdot:
+    def __init__(self,index):
+        self.connections=[]
+        self.maxstate=0
+        self.states=[]
+        self.state=[]
+        self.index=index
+        self.statenew=[]
+        self.momentstate=[]
+        self.statenewmoment=[]
+      
+
+    def sim(self,qdots):
+        self.stringstate=[]
+        self.statenew=self.state.copy()
+        self.statenewmoment=qdots[self.connections[0]].state.copy()
+        self.momentstate=[]
+
+        for i in range(1,len(self.connections)):
+
+            l=len(self.statenewmoment)
+
+            for j in range(l):
+                
+                for k in range(len(qdots[self.connections[i]].state)):
+                    self.momentstate.append(self.statenewmoment[j]*qdots[self.connections[i]].state[k])
+                
+                
+            l=len(self.statenewmoment)
+            self.statenewmoment=self.momentstate.copy()
+            self.momentstate=[]
+    
+        for i in range(len(self.statenew)):
+            self.statenew[i]=0
+  
+        for i in range(len(self.statenewmoment)):
+               
+
+            self.statenew[self.states[i]]+=self.statenewmoment[i]
+            
+        self.statenew=self.statenew/np.linalg.norm(self.statenew)
+          
+        return self.statenew            
+            
+
+class simqdot:
+
+    def simqdots(self,niter,qdots):
+        listofstates=[]
+        for i in range(niter):
+
+            
+            liststates=[]
+
+
+            for i in range(len(qdots)):
+                liststates.append(qdots[i].sim(qdots))
+ 
+            for i in range(len(qdots)):
+                qdots[i].state=liststates[i]
+            listofstates.append(liststates)
+        return listofstates
+
+    
+
+        
